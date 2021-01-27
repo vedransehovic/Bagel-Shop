87943bb406ee7f41f293f7648b842cd6e68d65e1
vedransehovic <vedran.sehovic@gmail.com>
2021-01-27 17:25:00 -0500

    redux state sorted out

--- restaurant_frontend/src/containers/BagelContainer.js
+++ restaurant_frontend/src/containers/BagelContainer.js
@@ -1,46 +1,15 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import AllBagels from "../components/AllBagels";
 
-class BagelContainer extends Component {
-  state = {
-    bagels: [],
-  };
-
-  componentDidMount() {
-    fetch("http://localhost:3000/api/v1/bagels")
-      .then((res) => res.json())
-      .then((data) => this.setState({ bagels: data }));
-  }
-
-  render() {
-    console.log(this.state.bagels);
-    console.log(this.props.bagels);
-    return (
-      <div>
-        <h1>This is Bagel Container</h1>
-        {this.state.bagels.map((bagel, index) => {
-          return (
-            <div>
-              <p key={index}>
-                {bagel.name}
-                <br />
-                <img
-                  key={index}
-                  src={bagel.image}
-                  alt={bagel.name}
-                  width="175px"
-                ></img>
-              </p>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+function BagelContainer() {
+  const bagels = useSelector((state) => state.bagels);
+  return (
+    <div>
+      <h1>This is BAGEL CONTAINER!</h1>;
+      <AllBagels />
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  bagels: state.bagels,
-});
-
-export default connect(mapStateToProps)(BagelContainer);
+export default BagelContainer;
